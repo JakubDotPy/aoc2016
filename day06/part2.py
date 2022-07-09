8209c46dcd812032a10a80d9c26e8d77cf5ddccb
Jakub Červinka <cervinka.jakub.1989@gmail.com>
2022-07-09 23:44:22 +0200

    day06 - part2

---
+++ day06/part2.py
@@ -0,0 +1,65 @@
+import argparse
+import collections
+import os.path
+
+import pytest
+
+from support import timing
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+# NOTE: paste test text here
+INPUT_S = """\
+eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar
+"""
+EXPECTED = 'advent'
+
+
+def compute(s: str) -> int:
+    columns = zip(*s.splitlines())
+    message = ''
+    for col in columns:
+        cnt = collections.Counter(col)
+        message += cnt.most_common()[-1][0]
+
+    return message
+
+
+@pytest.mark.solved
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+            ),
+    )
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
